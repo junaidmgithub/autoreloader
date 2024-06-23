46499b3701a1fb5999fc7c21249f7d0705733a77
junaid <39518060+junaidmgithub@users.noreply.github.com>
2024-06-23 19:26:34 +0530

    test files

---
+++ test/main.py
@@ -0,0 +1,13 @@
+from sub import Sub
+
+
+class Main:
+
+    def __init__(self) -> None:
+        self.sub = Sub()
+        print("Main Class")
+
+    def change():
+        print('working.')
+
+obj = Main()
---
+++ test/sub.py
@@ -0,0 +1,5 @@
+
+
+class Sub:
+    def __init__(self) -> None:
+        print("Sub Class.")
